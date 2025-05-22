4dfc83a98a0931ebf5de666624cac0fc0b6fce91
SacchXN <106769040+SacchXN@users.noreply.github.com>
2025-05-22 15:02:19 +0200

    dirty update

--- camera.py
+++ camera.py
@@ -1,4 +1,6 @@
+import queue
 import cv2 as cv
+import threading
 
 # Default values
 CAMERA_FRAME_WIDTH = cv.CAP_PROP_FRAME_WIDTH
@@ -10,23 +12,39 @@ class Capture:
         self.__width = width
         self.__height = height
 
-    def start_video(self):
+    def start_video(self, frame_queue: queue.Queue, landmark_queue: queue.Queue, stop_condition: threading.Event):
         if not self.__camera.isOpened():
             print("Error starting camera.")
             exit()
 
-        while True:
+        while not stop_condition.is_set():
             ret, frame = self.__camera.read()
 
             if not ret:
-                print("Error receiving frames.")
-                break
+                #("Error receiving frames.")
+                continue
+
+            # The timeout value actually stops the execution of the whole while loop therefore creating a video
+            # "freeze" of the duration of the timeout. It needs to be set as low as possible otherwise it doesn't
+            # feel smooth enough
+
+            try:
+                frame_queue.put(frame, timeout=0.1)
+            except queue.Full:
+                #print('Frame queue is full, skipped frame.')
+                pass
 
             #    In case a greyscale image is required
             #    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            cv.imshow('Video', frame)
+            cv.imshow('Frame', frame)
 
+            #if landmark_queue.empty():
+            #    print('Landmark queue is empty.')
+            #    continue
+
+            #cv.imshow('Landmark', landmark_queue.get())
             if cv.waitKey(1) == ord('q'):
+                stop_condition.set()
                 break
 
     def end_video(self):
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import camera
+import hand_detection
+import queue
+import threading
 
-video = camera.Capture()
-video.start_video()
-video.end_video()
+frame_queue = queue.Queue(maxsize=10)
+landmark_queue = queue.Queue(maxsize=10)
+stop_condition = threading.Event()
+
+#video = camera.Capture()
+#video_thread = threading.Thread(target=video.start_video, args=(frame_queue, landmark_queue, stop_condition))
+#video_thread.start()
+
+hand_detect = hand_detection.HandDetection()
+hand_thread = threading.Thread(target=hand_detect.start_detection, args=(frame_queue, landmark_queue, stop_condition))
+hand_thread.start()
+#hand_detect.start_detection(frame_queue, landmark_queue, stop_condition)
+
+#video_thread.join()
+#hand_thread.join()
+
+#video.end_video()
